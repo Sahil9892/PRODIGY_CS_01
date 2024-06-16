0057efb24c05657f71962c1bee84c4d92e886408
Sahil9892 <sahildorugade09@gmail.com>
2024-06-16 20:23:53 +0530

    .

---
+++ task1.py
@@ -0,0 +1,30 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def encrypt(norm_text, shift_amount):
+    cipher_text = ""
+    for letter in norm_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(f"The encoded text is {cipher_text}")
+
+def decrypt(cipher_text, shift_amount):
+    plain_text = ""
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        plain_text += alphabet[new_position]
+    print(f"The decoded text is {plain_text}")
+
+def main():
+    direction = input("Type encode to encrypt, decode to decrypt: ")
+    text = input("Type your message: ").lower()
+    shift = int(input("Type the shift number: "))
+
+    if direction == "encode":
+        encrypt(norm_text = text, shift_amount = shift)
+    else:
+        decrypt(cipher_text=text, shift_amount=shift)
+
+main()
